1ed633d71c2b1d6d0bd8b7f594b29b388accd08b
hsmtkk <hsmtkk@gmail.com>
2024-01-27 08:28:35 +0900

    add user schema

--- app/utils/schemaModels.ts
+++ app/utils/schemaModels.ts
@@ -3,11 +3,44 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    email: String,
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
 })
 
 export const ItemModel = mongoose.models.Item || mongoose.model("Item", ItemSchema)
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+export const UserModel = mongoose.models.User || mongoose.model("User", UserSchema)
