b2dd83e2e1fb4b0151d16f9a122e816bdd5c69e1
hsmtkk <hsmtkk@gmail.com>
2024-01-27 12:33:46 +0900

    save token when login

--- app/user/login/page.tsx
+++ app/user/login/page.tsx
@@ -3,30 +3,28 @@
 import { Button, Input, Spacer } from "@nextui-org/react";
 import { useState } from "react";
 
-export default function Regsiter() {
-    const [name, setName] = useState<string>("")
+export default function Login() {
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
 
     const handleClick = async () => {
-        const body = { name, email, password }
+        const body = { email, password }
         try {
-            const resp = await fetch("/api/user/register", {
+            const resp = await fetch("/api/user/login", {
                 method: "POST",
                 body: JSON.stringify(body),
             })
             const respJson = await resp.json()
+            localStorage.setItem("token", respJson.token)
             alert(respJson.message)
         } catch (err) {
             console.log(err)
-            alert("ユーザー登録失敗")
+            alert("ログイン失敗")
         }
     }
 
     return (
         <div className="container">
-            <Input label="名前" value={name} onValueChange={setName} required />
-            <Spacer />
             <Input label="メールアドレス" value={email} onValueChange={setEmail} required />
             <Spacer />
             <Input label="パスワード" type="password" value={password} onValueChange={setPassword} required />
